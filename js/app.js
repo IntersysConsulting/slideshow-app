4b92876ee6ab53b605c4011424bd427703b492b3
Edgardo Campos <xotl@Xotls-MacBook-Pro.local>
2018-08-07 18:42:35 -0500

    Improving app & bugfixes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
+var GITHUB_TOKEN = 'Insert your Github Token here';
+
+
 Vue.component('slideshow-media', {
     template:`
         <img v-if="mediaUrl !== null" v-bind:src="mediaUrl" class="media-component"/>
         <div v-else>
-            <h1>No images available</h1>
+            <h1 class="white-text">No images available</h1>
         </div> 
     `,
     props: ['mediaIndex', 'mediaUrls'],
@@ -24,7 +27,7 @@ var app = new Vue({
         message: 'This is a Vue App',
         currentMediaIndex: 0,
         imagesUrls: null,
-        showErrorMsg: false,
+        hasErrorMsg: null,
         slideShowTimer: null
     },
     created: function () { 
@@ -41,7 +44,7 @@ var app = new Vue({
         },
         startSlideshow: function() {
             this.slideShowTimer = setInterval(() => {
-                let newIndex = this.currentMediaIndex + 1
+                var newIndex = this.currentMediaIndex + 1
 
                 if ( newIndex >= this.imagesUrls.length ) {
                     newIndex = 0// Reset the slideshow
@@ -51,17 +54,34 @@ var app = new Vue({
             }, 5000)
         },
         getMediaUrls: function() {
-            fetch('https://api.github.com/repos/IntersysConsulting/slideshow-app/contents/media')
-                .then( res => res.json() )
-                .then( payload => {
-                    const newImagesUrls = payload.filter( item => item.type === 'file' ).map( item => item.download_url )
-                    if ( this.currentMediaIndex >= newImagesUrls.length ) {
-                        this.currentMediaIndex = 0// Reset the slideshow
-                    }
-                    this.imagesUrls = newImagesUrls
-                    this.showErrorMsg = false
-                } )
-                .catch( () => this.showErrorMsg = true )
-            }
+            var headers = new Headers({
+                Authorization: 'token' + GITHUB_TOKEN
+            });
+            fetch(
+                'https://api.github.com/repos/IntersysConsulting/slideshow-app/contents/media',
+                { 
+                    headers
+                }
+            )
+            .then( res => {
+                if (res.ok) {
+                    return res.json()
+                }
+                else {
+                    throw new Error(res.body)
+                }
+            } )
+            .then( payload => {
+                var newImagesUrls = payload.filter( item => item.type === 'file' ).map( item => item.download_url )
+                if ( this.currentMediaIndex >= newImagesUrls.length ) {
+                    this.currentMediaIndex = 0// Reset the slideshow
+                }
+                this.imagesUrls = newImagesUrls
+                this.hasErrorMsg = null
+            } )
+            .catch( err => {
+                this.hasErrorMsg = err
+            } )
+        }
     }
 })
